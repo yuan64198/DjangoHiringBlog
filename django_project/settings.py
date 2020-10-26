98d6571e972916854110173711565ac9641f4352
yuan64198 <yuan64198@gmail.com>
2020-10-25 23:33:48 -0500

    modify settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
