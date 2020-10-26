d256d7a89ea36ba66447db2f5f2dd741eae57d7d
yuan64198 <yuan64198@gmail.com>
2020-10-25 23:48:07 -0500

    123

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://mydjangoemploymentwebsite.herokuapp.com/']
+ALLOWED_HOSTS = ['https://mydjangohiringblog.herokuapp.com']
 
 
 # Application definition
